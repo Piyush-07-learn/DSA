234260714e012f1b97bd52083be3a1d6219176e9
Piyush Upadhyay <reachmeoutanytime@gmail.com>
2026-08-29 23:27:48 +0530

    feat(two-pointers): add Remove Duplicates solution
    Uses fast and slow pointers for in-place array modification

---
+++ 02_Two_Pointers/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        
+        int slow = 0;
+        for (int fast = 1; fast < nums.size(); fast++) {
+            if (nums[fast] != nums[slow]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+        return slow + 1; // Length of the array with unique elements
+    }
+};
