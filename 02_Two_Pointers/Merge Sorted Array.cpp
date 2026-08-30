0a320c6ef8a9b540437f04a66d3fdffd51d480ef
Piyush Upadhyay <reachmeoutanytime@gmail.com>
2026-08-30 23:33:10 +0530

    feat(two-pointers): add Merge Sorted Array
    Implements reverse two pointers to merge in-place without overwriting

---
+++ 02_Two_Pointers/Merge Sorted Array.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1; // Last actual element in nums1
+        int p2 = n - 1; // Last element in nums2
+        int p = m + n - 1; // End of the buffer in nums1
+        
+        while (p2 >= 0) {
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+    }
+};
